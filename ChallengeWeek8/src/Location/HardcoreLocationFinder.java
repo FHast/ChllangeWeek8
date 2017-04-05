d576b819f795e2b1afe602036357f29ed305b98a
gereon <gereon@gereonLaptop>
2017-04-05 09:20:54 +0200

    creates class

---
+++ ChallengeWeek8/src/Location/HardcoreLocationFinder.java
@@ -0,0 +1,14 @@
+package Location;
+
+import Utils.MacRssiPair;
+import Utils.Position;
+
+public class HardcoreLocationFinder implements LocationFinder {
+
+	@Override
+	public Position locate(MacRssiPair[] data) {
+		
+		return null;
+	}
+
+}
